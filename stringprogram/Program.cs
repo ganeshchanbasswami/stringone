0c97db24e8004bbdca2e5d65316bbc6ec4a1ad26
Admin <Admin@PRADEEPSWAMI12>
2023-02-24 17:14:10 +0530

    Add project files.

---
+++ stringprogram/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+   public class program
+{
+    public static void Main()
+    {
+        //string firstname, middlename, lastname, fullname;
+
+        //Console.WriteLine("enter firstname");
+        //firstname= Console.ReadLine();
+
+        //Console.WriteLine("entre middle name");
+        //middlename= Console.ReadLine();
+
+        //Console.WriteLine("enter last name");
+        //lastname= Console.ReadLine();
+
+        //fullname=  firstname +""+ middlename+""+lastname;
+        //Console.WriteLine(fullname);
+
+        Console.WriteLine("entre your first string");
+        var str = Console.ReadLine();
+
+
+        foreach (var item in str)
+       {
+            Console.WriteLine(item);
+        }
+            
+
+        
+        
+
+        
+    }
+}
